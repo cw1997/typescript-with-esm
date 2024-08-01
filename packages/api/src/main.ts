7134fff4e1e55b1229127b97665f88043276d18c
Chang Wei <changwei1006@gmail.com>
2024-08-01 16:56:32 +0800

    fix ci

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,7 +1,8 @@
 // import './paths.js'
 
 import chalk from "chalk";
-import {getUuid} from "src/utils/id.js";
+import {getUuid} from "@/utils/id.js";
+// import {getUuid} from "src/utils/id.js";
 // import {getUuid} from "./utils/id.js";
 
 export async function main() {

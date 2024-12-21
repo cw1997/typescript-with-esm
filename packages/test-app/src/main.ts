8be0c8d9fbbb5a82321a5ced9ca9c74d68711ee6
Chang Wei <changwei1006@gmail.com>
2024-12-22 05:42:24 +0800

    add type declaration to test-lib

--- packages/test-app/src/main.ts
+++ packages/test-app/src/main.ts
@@ -5,6 +5,12 @@ import {utils} from "@cw1997/test-lib"
 export async function main() {
   console.log('start')
   console.log(utils.add(1, 2))
+
+  const obj: utils.t_calculation = {
+    operator: "+",
+    operand: 1,
+  }
+  console.log(obj.operator)
 }
 
 await main()
--- packages/test-lib/src/utils.ts
+++ packages/test-lib/src/utils.ts
@@ -5,3 +5,8 @@ export const add = (a: number, b: number) => {
 export const sub = (a: number, b: number) => {
   return a - b;
 }
+
+export type t_calculation = {
+  operator: '+' | '-'
+  operand: number
+}
